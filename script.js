3e9d40bb14070257bf0829dc60713539b58cb460
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-03-15 10:44:45 +0700

    Update script.js
    รายละเอียด
    - แสดง
    - ลบ
    - แก้ไข(Rights ขึ้นค่าแรก ต้องแก้)
    - เพิ่ม

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ xhttp.onreadystatechange = function () {
         showUserCreateBox();
     }
 }
+
+function getRightOptionsHTML(selectedOption) {
+    let optionsHTML = "<option disabled value> -- เลือกตัวเลือก -- </option>";
+    for (let i = 0; i < rightList.length; i++) {
+        if (rightList[i].Patient_Rights === selectedOption) { // Access data using key
+            optionsHTML += `<option value="${rightList[i].Patient_Rights}" selected>${rightList[i].Patient_Rights}</option>`;
+        } else {
+            optionsHTML += `<option value="${rightList[i].Patient_Rights}">${rightList[i].Patient_Rights}</option>`;
+        }
+    }
+    return optionsHTML;
+}
 //////////////////////////////////// show right options ///////////////////////////////////
 
 //////////////////////////////////// Load Table ///////////////////////////////////
@@ -45,6 +57,10 @@ function loadTable() {
             }
 
             document.getElementById("patientTable").innerHTML = trHTML;
+        } else {
+            var trHTML = "";
+            trHTML += "<tr><td> 404 Server not found </td></tr>";
+            document.getElementById("patientTable").innerHTML = trHTML;
         }
     };
 }
@@ -53,6 +69,53 @@ loadTable();
 //////////////////////////////////// Load Table ///////////////////////////////////
 
 //////////////////////////////////// SEARCH ///////////////////////////////////
+
+function patientSearch() {
+    let searchText = document.getElementById("KeywordSearch").value;
+    console.log(searchText)
+    if (searchText == "") {
+        loadTable()
+    } else {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "http://localhost:3000/patients/" + searchText);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                var trHTML = "";
+                var num = 1;
+                const objects = JSON.parse(this.responseText);
+                for (let object of objects) {
+                    trHTML += "<tr>";
+                    trHTML += "<td>" + num + "</td>";
+                    trHTML += "<td>" + object["HN"] + "</td>";
+                    trHTML += "<td>" + object["Name"] + "</td>";
+                    trHTML += "<td>" + object["Patient_Rights_1"] + "</td>";
+                    trHTML += "<td>" + object["Patient_Rights_2"] + "</td>";
+                    trHTML += "<td>" + object["Patient_Rights_3"] + "</td>";
+                    trHTML += "<td>" + object["Chronic_Disease"] + "</td>";
+                    trHTML += "<td>" + object["Address"] + "</td>";
+                    trHTML += "<td>" + object["Phone"] + "</td>";
+                    trHTML += '<td><i class ="btn text-primary bi bi-pencil-square" onclick = "showEditbox(' + object["ID"] + ')"></i>';
+                    trHTML += '<i class = "btn text-danger bi bi-trash3-fill" onclick = "patientDelete(' + object["ID"] + ')"></i></td>';
+                    trHTML += "</tr>";
+
+                    num++;
+                    lastestHN = object["HN"];
+                }
+
+                document.getElementById("patientTable").innerHTML = trHTML;
+
+            } else {
+                var trHTML = "";
+                trHTML += "<tr><td> Data not found </td></tr>";
+
+                document.getElementById("patientTable").innerHTML = trHTML;
+            }
+        };
+        xhttp.send();
+    }
+}
 
 //////////////////////////////////// SEARCH ///////////////////////////////////
 
@@ -67,15 +130,12 @@ function patientDelete(id) {
         "patientID": id  // Corrected from patientID to id
     }));
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                const objects = JSON.parse(this.responseText);
-                Swal.fire("Good job!", "ลบข้อมูลผู้ป่วยสำเร็จ", "success");
-                loadTable();
-            } else {
-                Swal.fire("Failed!", "ลบข้อมูลผู้ป่วยไม่สำเร็จ", "error");
-                loadTable();
-            }
+        if (this.readyState == 4 && this.status == 200) {
+            Swal.fire("Good job!", "ลบข้อมูลผู้ป่วยสำเร็จ", "success");
+            loadTable();
+        } else {
+            Swal.fire("Failed!", "ลบข้อมูลผู้ป่วยไม่สำเร็จ", "error");
+            loadTable();
         }
     };
 }
@@ -96,29 +156,29 @@ function showEditbox(id) {
             Swal.fire({
                 title: 'แก้ไขข้อมูลผู้ป่วย',
                 html:
-                '<div class="mb-3"><label for="HN" class="form-label float-start">HN</label>' +
-                '<input class="form-control" id="HN" value="' + patientData["HN"] + '" disabled></div>' +
+                    '<div class="mb-3"><label for="HN" class="form-label float-start">HN</label>' +
+                    '<input class="form-control" id="HN" value="' + patientData["HN"] + '" disabled></div>' +
 
-                '<div class="mb-3"><label for="Name" class="form-label float-start">ชื่อ-สกุลผู้ป่วย</label>' +
-                '<input class="form-control" id="Name" value="' + patientData["Name"] + '"></div>' +
+                    '<div class="mb-3"><label for="Name" class="form-label float-start">ชื่อ-สกุลผู้ป่วย</label>' +
+                    '<input class="form-control" id="Name" value="' + patientData["Name"] + '"></div>' +
 
-                '<div class="mb-3"><label for="Right1" class="form-label float-start">สิทธิการรักษา 1</label>' +
-                '<select class="form-select form-select-sm" id="Right1">' + getRightOptionsHTML('Right1', patientData["Right1"]) + '</select></div>' +
+                    '<div class="mb-3"><label for="Right1" class="form-label float-start">สิทธิการรักษา 1</label>' +
+                    '<select class="form-select form-select-sm" id="Right1">' + getRightOptionsHTML('Right1', patientData["Right1"]) + '</select></div>' +
 
-                '<div class="mb-3"><label for="Right2" class="form-label float-start">สิทธิการรักษา 2</label>' +
-                '<select class="form-select form-select-sm" id="Right2">' + getRightOptionsHTML('Right2', patientData["Right2"]) + '</select></div>' +
+                    '<div class="mb-3"><label for="Right2" class="form-label float-start">สิทธิการรักษา 2</label>' +
+                    '<select class="form-select form-select-sm" id="Right2">' + getRightOptionsHTML('Right2', patientData["Right2"]) + '</select></div>' +
 
-                '<div class="mb-3"><label for="Right3" class="form-label float-start">สิทธิการรักษา 3</label>' +
-                '<select class="form-select form-select-sm" id="Right3">' + getRightOptionsHTML('Right3', patientData["Right3"]) + '</select></div>' +
+                    '<div class="mb-3"><label for="Right3" class="form-label float-start">สิทธิการรักษา 3</label>' +
+                    '<select class="form-select form-select-sm" id="Right3">' + getRightOptionsHTML('Right3', patientData["Right3"]) + '</select></div>' +
 
-                '<div class="mb-3"><label for="Chronic" class="form-label float-start">โรคประจำตัว</label>' +
-                '<input class="form-control" id="Chronic" value="' + patientData["Chronic_Disease"] + '"></div>' +
+                    '<div class="mb-3"><label for="Chronic" class="form-label float-start">โรคประจำตัว</label>' +
+                    '<input class="form-control" id="Chronic" value="' + patientData["Chronic_Disease"] + '"></div>' +
 
-                '<div class="mb-3"><label for="Address" class="form-label float-start">ที่อยู่</label>' +
-                '<input class="form-control" id="Address" value="' + patientData["Address"] + '"></div>' +
+                    '<div class="mb-3"><label for="Address" class="form-label float-start">ที่อยู่</label>' +
+                    '<input class="form-control" id="Address" value="' + patientData["Address"] + '"></div>' +
 
-                '<div class="mb-3"><label for="Phone" class="form-label float-start">เบอร์ติดต่อ</label>' +
-                '<input class="form-control" id="Phone" value="' + patientData["Phone"] + '"></div>',
+                    '<div class="mb-3"><label for="Phone" class="form-label float-start">เบอร์ติดต่อ</label>' +
+                    '<input class="form-control" id="Phone" value="' + patientData["Phone"] + '"></div>',
 
                 focusConfirm: false,
                 showCancelButton: true,
@@ -146,7 +206,7 @@ function showEditbox(id) {
 
                     };
 
-                    xhttpUpdate.open("PUT", `http://localhost:3000/patients/update/${id}`);
+                    xhttpUpdate.open("PUT", `http://localhost:3000/patients/update/` + id);
                     xhttpUpdate.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
                     xhttpUpdate.send(JSON.stringify({
                         'HN': HN,
@@ -265,14 +325,3 @@ function createNewPatient() {
     };
 }
 
-function getRightOptionsHTML(selectedOption) {
-    let optionsHTML = "<option disabled value> -- เลือกตัวเลือก -- </option>";
-    for (let i = 0; i < rightList.length; i++) {
-        if (rightList[i].Patient_Rights === selectedOption) { // Access data using key
-            optionsHTML += `<option value="${rightList[i].Patient_Rights}" selected>${rightList[i].Patient_Rights}</option>`;
-        } else {
-            optionsHTML += `<option value="${rightList[i].Patient_Rights}">${rightList[i].Patient_Rights}</option>`;
-        }
-    }
-    return optionsHTML;
-}
